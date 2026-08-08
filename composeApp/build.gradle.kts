3c05b93e1d59d72ed26822c3199eea302829ac5e
EternalFuture゙ <142726636+eternalfuture-e38299@users.noreply.github.com>
2026-08-09 00:32:45 +0800

    Set signing config for release build type

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -167,6 +167,7 @@ androidApp.apply {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
